40662e69f006f7b7494b112117c63c4ab7c0eb6a
BettinaP <bettinaprophete@gmail.com>
2018-03-11 20:37:02 -0400

    added log in page UI

--- Twitter_ProjDemo/ViewController.swift
+++ Twitter_ProjDemo/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var submitOrLoginButton: UIButton!
+    @IBOutlet weak var changeModeButton: UIButton!
+    @IBOutlet weak var changeModeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func submitOrLoginTapped(_ sender: UIButton) {
+   
+    }
+    
 }
 
